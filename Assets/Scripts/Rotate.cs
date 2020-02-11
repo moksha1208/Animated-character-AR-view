ae199719ed5d0451e286ad39caa1e24a294b5878
jhanvishah <jhanvishah26@gmail.com>
2020-02-11 21:40:56 +0530

    added

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	Vector3 vect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vect = new Vector3(50, 50, 50);
+		transform.Rotate(vect * Time.deltaTime);
+    }
+}
